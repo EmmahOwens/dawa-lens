a3c3986d5b0e9fdd80b3375f6d214a1290554fc1
EmmahOwens <emmahmbayo@gmail.com>
2026-04-12 07:50:51 +0300

    dawalens.web.app fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyC6oGg9qudi-L6umqGd9hoX-0OPvmTQHhU",
-    authDomain: "medicine-d3ba2.firebaseapp.com",
+    authDomain: "dawalens.firebase.app",
     projectId: "medicine-d3ba2",
     storageBucket: "medicine-d3ba2.firebasestorage.app",
     messagingSenderId: "78961271210",
