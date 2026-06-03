d9439ad564eecf9be3f0cfa436a49cbc1d226915
EmmahOwens <emmahmbayo@gmail.com>
2026-06-03 14:46:45 +0300

    refactor: update Rive initialization to use the new Rive.init method

--- android/app/src/main/java/com/dawainnovation/lens/DawaLensApplication.kt
+++ android/app/src/main/java/com/dawainnovation/lens/DawaLensApplication.kt
@@ -1,12 +1,12 @@
 package com.dawainnovation.lens
 
 import android.app.Application
-import app.rive.runtime.kotlin.RiveInitializer
+import app.rive.runtime.kotlin.core.Rive
 
 class DawaLensApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // Initialize Rive
-        RiveInitializer.initializer(this)
+        Rive.init(this)
     }
 }
