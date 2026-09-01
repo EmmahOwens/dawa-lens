40dc0a17e10572f350b8ab7e469388e07708ec65
EmmahOwens <emmahmbayo@gmail.com>
2026-09-01 15:25:53 +0300

    feat: add network security configuration for Android application

--- rust/target/debug/deps/dawa_search-2f36adde190a0bc6.d
+++
@@ -1,6 +0,0 @@
-/home/iammbayo/Documents/Projects/dawa-lens/rust/target/debug/deps/dawa_search-2f36adde190a0bc6.d: src/lib.rs src/search.rs
-
-/home/iammbayo/Documents/Projects/dawa-lens/rust/target/debug/deps/libdawa_search-2f36adde190a0bc6.rmeta: src/lib.rs src/search.rs
-
-src/lib.rs:
-src/search.rs:
